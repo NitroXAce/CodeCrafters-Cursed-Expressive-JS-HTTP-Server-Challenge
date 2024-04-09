0e77d8171fa4a8780323a52d6fcd1bab57e38967
Gareth Compton <gearheadgareth@outlook.com>
2024-04-09 05:16:06 -0400

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -6,41 +6,38 @@
             data,
             message = data.toString().split("\r\n"),
             path = message[0].split(' ')[1]
-        )=>(
+        )=>socket.end((
             console.log(data.toString(),`\nPath: '${path}'`),(
-                stringToObj(message[0],{
-                    GET:{
-                        [path?.[1] ?? '/']:{
-                            'HTTP/1.1':true
-                        }
+            stringToObj(message[0],{
+                GET:{
+                    [path?.[1] ?? '/']:{
+                        'HTTP/1.1':true
                     }
-                }) &&
-                stringToObj(message[1],{
-                    'Host:':{
-                        'localhost:4221':true
-                    }
-                }) &&
-                stringToObj(message[2],{
-                    'User-Agent:':{
-                        'Go-http-client/1.1':()=>(
-                            socket.end(
-                                socket.write(`${[
-                                    'HTTP/1.1 200 OK',
-                                    'Content-Type: text/plain',
-                                    'Content-Length: 3',
-                                    '',
-                                    'abc'
-                                ].join('\r\n')}`)
-                            )
-                        )
-                    }
-                })
-            ) ?? socket.end()
+                }
+            }) &&
+            stringToObj(message[1],{
+                'Host:':{
+                    'localhost:4221':true
+                }
+            }) &&
+            stringToObj(message[2],{
+                'User-Agent:':{
+                    'Go-http-client/1.1':()=>
+                        socket.write(`${[
+                            'HTTP/1.1 200 OK',
+                            'Content-Type: text/plain',
+                            'Content-Length: 3',
+                            '',
+                            'abc'
+                        ].join('\r\n')}`)
+                }
+            })
+        )
             /*path === "/"
             ? socket.write("HTTP/1.1 200 OK\r\n\r\n")
             : socket.write("HTTP/1.1 404 Not Found\r\n\r\n"),*/
             //socket.end()
-        )),
+        ))),
         socket.on("close", () => (
             socket.end(),
             server.close()
