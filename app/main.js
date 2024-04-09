36e60a806c9abb01e0ddf06ab53c077750cf01bf
Gareth Compton <gearheadgareth@outlook.com>
2024-04-09 04:50:19 -0400

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -23,14 +23,15 @@
                 match(message[2],{
                     'User-Agent:':{
                         'Go-http-client/1.1':()=>(
-                            socket.write(`${[
-                                'HTTP/1.1 200 OK',
-                                'Content-Type: text/plain',
-                                'Content-Length: 3',
-                                '',
-                                'abc'
-                            ].join('\r\n')}\r\n`),
-                            socket.end()
+                            socket.end(
+                                socket.write(`${[
+                                    'HTTP/1.1 200 OK',
+                                    'Content-Type: text/plain',
+                                    'Content-Length: 3',
+                                    '',
+                                    'abc'
+                                ].join('\r\n')}\r\n`)
+                            )
                         )
                     }
                 })
