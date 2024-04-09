d2578bf8b0a580acb04db9dd6ba9ba0046fa06a8
Gareth Compton <gearheadgareth@outlook.com>
2024-04-09 02:05:05 -0400

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -1,12 +1,12 @@
 ((
     net = require('net'),
-    server = net.createServer((socket) => (
+    server = net.createServer(socket => (
         socket.on('data', (
             data,
             message = data.toString().split("\r\n"),
             path = message[0].split(' ')[1]
         )=>(
-            console.log(`Path: '${path}'`),
+            console.log(data,`\nPath: '${path}'`),
             path === "/"
             ? socket.write("HTTP/1.1 200 OK\r\n\r\n")
             : socket.write("HTTP/1.1 404 Not Found\r\n\r\n"),
