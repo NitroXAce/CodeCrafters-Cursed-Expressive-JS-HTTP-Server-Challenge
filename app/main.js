88f33d639764f802df32fab78e68d4a3e465d5ae
Gareth Compton <gearheadgareth@outlook.com>
2024-04-08 21:13:43 -0400

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -1,11 +1,11 @@
 ((
     net = require('net'),
-    server = net.createServer((socket) => {
-        socket.on("close", () => {
-          socket.end();
-          server.close();
-        });
-      })
+    server = net.createServer((socket) => 
+        socket.on("close", () => (
+            socket.end(),
+            server.close()
+        ))
+    )
 )=>(
     server.listen(4221, "localhost")
 ))()
