952d5123e732ba13c2ded08617ea18f4b87b10e4
Gareth Compton <gearheadgareth@outlook.com>
2024-04-10 17:18:40 -0400

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -15,7 +15,7 @@
                         'HTTP/1.1 200 OK\r\n',
                         `Content-Type: text/plain\r\n`,
                         `Content-length: ${chunks.join('/').length -1}\r\n`,
-                        '',
+                        '\r\n',
                         '',
                         chunks.join('/')
                     ].join('')
