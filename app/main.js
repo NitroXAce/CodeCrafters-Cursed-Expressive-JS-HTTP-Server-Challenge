c9e755c6f54b5bf204817c7b8e5f104b6d4c87a2
Gareth Compton <gearheadgareth@outlook.com>
2024-04-10 16:34:18 -0400

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -8,7 +8,18 @@
             path = message[0].split(' ')[1]
         )=>socket.end((
             console.log(data.toString(),`\nPath: '${path}'`),(
-            stringToObj(message,{
+            socketWrite(match(path,{
+                default:"HTTP/1.1 404 Not Found\r\n\r\n",
+                path:[
+                    'HTTP/1.1 200 OK',
+                    'Content-Type: text/plain',
+                    'Content-Length: 3',
+                    '',
+                    'abc'
+                ].join('\r\n'),
+
+            }))
+            /*stringToObj(message,{
                 ['GET ' + (path ?? '/') + ' HTTP/1.1']:{
                     'Host: localhost:4221':{
                         'User-Agent: Go-http-client/1.1':()=>
@@ -21,7 +32,7 @@
                             ].join('\r\n')}`)
                     }
                 }
-            })
+            }) ?? socket.write("HTTP/1.1 404 Not Found\r\n\r\n")*/
         )
             /*path === "/"
             ? socket.write("HTTP/1.1 200 OK\r\n\r\n")
