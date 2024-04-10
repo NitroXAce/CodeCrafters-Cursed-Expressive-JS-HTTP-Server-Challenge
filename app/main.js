bb355e1dec5bd2bb3a9bff016aaa01ca18a7e9b6
Gareth Compton <gearheadgareth@outlook.com>
2024-04-10 17:11:56 -0400

    pass 4th stage

--- app/main.js
+++ app/main.js
@@ -12,10 +12,12 @@
                 match(path,{
                     '/':'HTTP/1.1 200 OK\r\n\r\n',
                     [`/echo/${chunks.join('/')}`]:[
-                        'HTTP/1.1 200 OK',
-                        `Content-Type: text/plain`,
-                        `Content-length: ${chunks.join('/').length}`
-                    ].join('\r\n')+chunks.join('/')
+                        'HTTP/1.1 200 OK\r\n',
+                        `Content-Type: text/plain\r\n`,
+                        `Content-length: ${chunks.join('/').length}\r\n`,
+                        '',
+                        chunks.join('/')
+                    ].join('')
                 })??
                 "HTTP/1.1 404 Not Found\r\n\r\n"
             )
