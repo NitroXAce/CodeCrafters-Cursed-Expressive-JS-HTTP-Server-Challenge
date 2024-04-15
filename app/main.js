919b13e801307cc09be17029d62d5b74901ed7b5
Ace Tributon <gearheadgareth@outlook.com>
2024-04-14 20:18:47 -0400

    pass 6th stage

--- app/main.js
+++ app/main.js
@@ -9,16 +9,14 @@
             [Host,address]=host.split(' '),
             [userAgent, Agent]=agent.split(' '),
             [begin,yeet,...chunks] = path.split('/'),
-        )=>socket.end(
+        )=>socket.end(socket.write(
             match(verb,{
-                GET:()=>socket.write(
-                    match(path,{
-                        '/':'HTTP/1.1 200 OK\r\n\r\n',
-                        [`/echo/${chunks.join('/')}`]:responseBody(chunks.join('/')),
-                        '/user-agent':responseBody(Agent)
-                    }) ?? "HTTP/1.1 404 Not Found\r\n\r\n"
-                )
-            })
+                GET:()=> match(path,{
+                    '/':'HTTP/1.1 200 OK\r\n\r\n',
+                    [`/echo/${chunks.join('/')}`]:responseBody(chunks.join('/')),
+                    '/user-agent':responseBody(Agent)
+                })
+            }) ?? "HTTP/1.1 404 Not Found\r\n\r\n")
         )),
         socket.on("close", () => (
             socket.end(),
