29ba3802500d35c759f0da4b0a49f94c2868324e
Ace Tributon <gearheadgareth@outlook.com>
2024-04-17 23:05:52 -0400

    pass 6th stage

--- app/modules.js
+++ app/modules.js
@@ -13,23 +13,21 @@
             firstArr in nestObj,
             'nestObj?.[' + firstArr + ']'
         ),
-        match(
-            typeof nestObj?.[firstArr],{
-                function:()=>nestObj[firstArr](),
-                object:()=>rest.length && (
-                    rest.join(splitter),
-                    stringToObj(rest, nestObj[firstArr])
-                ),
-                default: nestObj?.[firstArr]
-            }
-        )
+        match( typeof nestObj?.[firstArr],{
+            function:()=>nestObj[firstArr](),
+            object:()=>rest.length && (
+                rest.join(splitter),
+                stringToObj(rest, nestObj[firstArr])
+            ),
+            default: nestObj?.[firstArr]
+        })
     ))(),
     responseBody = send => match(typeof send,{
-        number:`HTTP/1.1 ${match(send,{
-            200: '200 OK',
-            404: "404 Not Found",
-            500: "500 Internal Server Error"
-        })}\r\n\r\n`,
+        number:match(send,{
+            200: 'HTTP/1.1 200 OK\r\n\r\n',
+            404: "HTTP/1.1 404 Not Found\r\n\r\n",
+            500: "HTTP/1.1 500 Internal Server Error\r\n\r\n",
+        }),
         string: send?.length && [
             'HTTP/1.1 200 OK\r\n',
             `Content-Type: text/plain\r\n`,
